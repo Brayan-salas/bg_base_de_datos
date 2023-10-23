a6a39390419a414282e40fc6c3fe5f2b232386bf
Brayan-salas <143459330+Brayan-salas@users.noreply.github.com>
2023-10-23 10:53:58 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
-import './App.css';
 import React from 'react';
-import { DataBaseTitle } from './resources/DataBaseTitle';
-import { UserName } from './resources/UserName';
-import { UserPassword } from './resources/UserPassword';
-import { LoginButton } from './resources/LoginButton';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+const MainPage = () => (
+  <div>
+    <header>
+      <h1>BG Datos y Cuentas</h1>
+    </header>
+    <main>
+      <section>
+        <h2>Venta de Datos</h2>
+        <p>Aquí puedes comprar datos de alta calidad para tus necesidades.</p>
+        <Link to="/datos">Ver Datos</Link>
+      </section>
+      <section>
+        <h2>Comprar Cuentas</h2>
+        <p>Accede a cuentas premium y servicios exclusivos.</p>
+        <Link to="/cuentas">Ver Cuentas</Link>
+      </section>
+    </main>
+  </div>
+);
+
+const DataPage = () => (
+  <div>
+    <h2>Página de Datos</h2>
+    {/* Contenido de la página de datos */}
+    <Link to="/">Volver a la página principal</Link>
+  </div>
+);
+
+const AccountsPage = () => (
+  <div>
+    <h2>Página de Cuentas</h2>
+    {/* Contenido de la página de cuentas */}
+    <Link to="/">Volver a la página principal</Link>
+  </div>
+);
 
 function App() {
   return (
-    <>
-      <DataBaseTitle/>
-      <UserName/>
-      <UserPassword/>
-      <LoginButton/>
-    </>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/datos" component={DataPage} />
+          <Route path="/cuentas" component={AccountsPage} />
+          <Route path="/" component={MainPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
